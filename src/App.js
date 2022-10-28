dc07de29eb804ff98268d721475dbdecf52dd052
Angela Choi <angelamchoi@gmail.com>
2022-10-28 13:36:59 -0400

    added card images and created shuffle cards function

--- src/App.js
+++ src/App.js
@@ -1,23 +1,43 @@
-import logo from './logo.svg';
 import './App.css';
+import {useState, useEffect} from "react";
+import axios, { Axios } from "axios";
+
+const cardImages = [
+  {"src": "/img/baby-yoda.webp"},
+  {"src": "/img/among-us.webp"},
+  {"src": "/img/cupcake-cat.jpg"},
+  {"src": "/img/donut-space.jpg"},
+  {"src": "/img/narwhal-gif.gif"},
+  {"src": "/img/nyan-cat-space.png"},
+  {"src": "/img/nyan-pikachu.jpg"},
+  {"src": "/img/pizza-gif.gif"},
+  {"src": "/img/snorlax-space.png"},
+  {"src": "/img/space-cat-burger.jpg"},
+  {"src": "/img/space-cat-pizza-taco.jpg"},
+  {"src": "/img/spacecat-gif.gif"},
+]
 
 function App() {
+  // STATES
+  const [cards, setCards] = useState([])
+  const [turns, setTurns] = useState(0)
+
+  //shuffle cards
+  const shuffleCards = () => {
+    const shuffledCards = [...cardImages, ...cardImages] // creates double cards
+    .sort(() => Math.random() - 0.5)
+    .map((card) =>({...card, id: Math.random()})) // make a new array for sorted cards & create ID for cards
+
+    setCards(shuffledCards)
+    setTurns(0)
+  }
+
+  console.log(cards,turns)
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+    <h1>Galaxy Matching Game</h1>
+    <button onClick={shuffleCards}> New Game</button>
     </div>
   );
 }
